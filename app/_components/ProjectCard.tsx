3eab9d2ac408f8eff103aa2a4a7d08e074e6244b
daniel <dvidalriezu@gmail.com>
2025-11-17 18:39:57 +0100

    project items contain image

--- app/_components/ProjectCard.tsx
+++ app/_components/ProjectCard.tsx
@@ -25,7 +25,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
                     src={project.image}
                     alt={project.title}
                     fill
-                    className="object-cover transition-transform duration-300 group-hover:scale-105"
+                    className="object-contain transition-transform duration-300 group-hover:scale-105"
                     unoptimized
                 />
             </div>
